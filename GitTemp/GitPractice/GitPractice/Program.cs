e40f9a72e97739808cf3a67033bb61f88864bac6
Acapellia <longhchim@naver.com>
2020-08-08 09:40:30 +0900

    GitPractice_Calculator

--- GitTemp/GitPractice/GitPractice/Program.cs
+++ GitTemp/GitPractice/GitPractice/Program.cs
@@ -1,9 +1,30 @@
 ﻿using System;
 
 namespace GitPractice {
+    class Calculator {
+        public int Add(int a, int b) {
+            return a + b;
+        }
+        public int Sub(int a,int b) {
+            return a - b;
+        }
+        public int Mul(int a,int b) {
+            return a * b;
+        }
+        public int Div(int a,int b) {
+            if(b != 0) {
+                return a / b;
+            }
+            return 0;
+        }
+    }
     class Program {
         static void Main(string[] args) {
-
+            Calculator c = new Calculator();
+            Console.WriteLine(c.Add(6, 3));
+            Console.WriteLine(c.Sub(6, 3));
+            Console.WriteLine(c.Mul(6, 3));
+            Console.WriteLine(c.Div(6, 3));
         }
     }
 }

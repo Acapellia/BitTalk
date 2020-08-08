cdf69820bc4854fbadabef056bc005a636ec23e7
helirang <66342017+helirang@users.noreply.github.com>
2020-08-08 10:14:06 +0900

    testBRA

--- GitTemp/GitPractice/GitPractice/Program.cs
+++ GitTemp/GitPractice/GitPractice/Program.cs
@@ -1,26 +1,35 @@
 ﻿using System;
 
-namespace GitPractice {
-    class Calculator {
-        public int Add(int a, int b) {
+namespace GitPractice
+{
+    //test_branch
+    class Calculator
+    {
+        public int Add(int a, int b)
+        {
             return a + b;
         }
-        public int Sub(int a, int b) {
+        public int Sub(int a, int b)
+        {
             return a - b;
         }
-        public int Mul(int a, int b) {
+        public int Mul(int a, int b)
+        {
             return a * b;
         }
-        public int Div(int a, int b) {
-            if(b != 0) {
+        public int Div(int a, int b)
+        {
+            if (b != 0)
+            {
                 return a / b;
             }
             return 0;
         }
     }
-    //branch
-    class Program {
-        static void Main(string[] args) {
+    class Program
+    {
+        static void Main(string[] args)
+        {
             Calculator c = new Calculator();
             Console.WriteLine(c.Add(6, 3));
             Console.WriteLine(c.Sub(6, 3));
